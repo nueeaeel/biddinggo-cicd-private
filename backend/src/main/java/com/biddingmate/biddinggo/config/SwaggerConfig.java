ee4346be5102259e4183dccd403122d617b95719
nueeaeel <dodms1006@naver.com>
2026-05-06 02:03:00 +0900

    feat: 도메인 변경 및 인증서 설정 추가

--- backend/src/main/java/com/biddingmate/biddinggo/config/SwaggerConfig.java
+++ backend/src/main/java/com/biddingmate/biddinggo/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,12 +22,15 @@ import java.util.List;
         )
 )
 public class SwaggerConfig {
+    @Value("${OPENAPI_SERVER_URL:http://localhost:8080}")
+    private String openApiServerUrl;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .servers(
                         List.of(
-                                new Server().url("http://localhost:8080")
+                                new Server().url(openApiServerUrl)
                         )
                 )
                 .components(
